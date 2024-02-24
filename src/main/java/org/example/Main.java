314d5059322f102eac848da6c8c77de89d9415b9
Lyudmila_Selezneva <lsarelli9402@gmail.com>
2024-02-24 12:42:13 +0400

    initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void printHello() {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class MainTest {
+
+    @Test
+    public void testPrintHello() {
+        Main.printHello();
+    }
+}
